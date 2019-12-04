f4dc1f4d028e73117f982769c6c3c6b19f581f86
Christopher Clemons <chris.clemons85@gmail.com>
2019-12-04 01:37:29 -0800

    fix createWork response object

--- app/contollers/work.controller.js
+++ app/contollers/work.controller.js
@@ -3,16 +3,20 @@ const Work = require('../models/work.model');
 module.exports.createWork = async (req, res, next) => {
   // const { userId } = req;
   const userId = '0a1e4fdd-28c8-4c84-a7e8-db9e22602ed2'; // Mock userId
-  const { projectId } = req.body;
+  const { projectId, date, start, end, duration } = req.body;
 
   let newWork;
   try {
     newWork = await new Work({
       userId,
-      projectId
+      projectId,
+      date,
+      start,
+      end,
+      duration
     }).save();
 
-    res.json({ work: newWork });
+    res.json(newWork);
   } catch (err) {
     console.error(err);
     next(err);
