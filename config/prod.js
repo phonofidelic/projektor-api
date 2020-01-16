e14dacea0fe222a9cd6261bfda6c0ba5146ee234
Christopher Clemons <chris.clemons85@gmail.com>
2020-01-15 18:48:27 -0800

    add missing prod config keys

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,8 @@ module.exports = {
   JWT_EXP: '15m',
   JWT_AUD: 'projektor_client',
   JWT_ISS: 'projektor_api',
+  SES_KEY: process.env.SES_KEY,
+  SES_SECRET: process.env.SES_SECRET,
   DOMAIN: 'https://projektor-api.herokuapp.com',
   CLIENT_DOMAIN: 'https://www.projektorapp.com'
 };
