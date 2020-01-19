8fea1921711f2768ecf7847402f0ad522d54ca33
phonofidelic <chris.clemons85@gmail.com>
2020-01-18 16:22:43 -0800

    add requireVerification middleware to all project routes

--- app/routes/project.routes.js
+++ app/routes/project.routes.js
@@ -12,12 +12,32 @@ const requireAuth = require('../middlewares/requireAuth');
 const requireVerification = require('../middlewares/requireVerification');
 
 router.get('/', requireAuth, requireVerification, getProjects);
-router.post('/create', requireAuth, createProject);
-router.get('/:projectId', requireAuth, getProject);
-router.put('/:projectId', requireAuth, editProject);
-router.put('/:projectId/status/delete', requireAuth, deleteProject);
-router.put('/:projectId/status', requireAuth, setProjectStatus);
-router.delete('/removed/delete/:projectId', requireAuth, deleteProject);
-router.delete('/removed/delete', requireAuth, deleteAllTrash);
+router.post('/create', requireAuth, requireVerification, createProject);
+router.get('/:projectId', requireAuth, requireVerification, getProject);
+router.put('/:projectId', requireAuth, requireVerification, editProject);
+router.put(
+  '/:projectId/status/delete',
+  requireAuth,
+  requireVerification,
+  deleteProject
+);
+router.put(
+  '/:projectId/status',
+  requireAuth,
+  requireVerification,
+  setProjectStatus
+);
+router.delete(
+  '/removed/delete/:projectId',
+  requireAuth,
+  requireVerification,
+  deleteProject
+);
+router.delete(
+  '/removed/delete',
+  requireAuth,
+  requireVerification,
+  deleteAllTrash
+);
 
 module.exports = router;
